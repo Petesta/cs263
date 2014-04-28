7b5a2d249ab833ed79f5a3a9b1dce96ec113b1fe
Petesta <Petesta@live.com>
2014-04-28 14:36:48 -0700

    Literal instruction complete.

--- src/SimpleVM/Parser/Parser.hs
+++ src/SimpleVM/Parser/Parser.hs
@@ -4,24 +4,28 @@ import Control.Monad.State
 
 --data InterpSTate = IS { stack :: [Value], pc :: Int }
 
-data Instr = Self | Literal | Send | SelfSend | SuperSend | Delegate | NonLocalReturn | IdxExtension
-data Value = SelfV | Index Int
+data Instr = Self | Literal Value | Send | SelfSend | SuperSend | Delegate | NonLocalReturn | IdxExtension
+data Value = SelfV | Lit LitVal | Index Int
+data LitVal = IntLit Int | StringLit String
 
 type Stack a = [a]
-type Interpreter a = State InterpState a
 type InterpState = [Value]
-
-handleInstruction :: Instr -> Interpreter ()
-handleInstruction instr =
-    case instr of
-        Self -> handleSelf
-        _ -> error "not implemented"
-        -- Literal -> handlerLiteral
-
-handleSelf :: Interpreter ()
-handleSelf = push SelfV
+type Interpreter a = State InterpState a
 
 push :: Value -> Interpreter ()
 push v = do
     stack <- get
     put (v:stack)
+
+handleInstruction :: Instr -> Interpreter ()
+handleInstruction instr =
+    case instr of
+        Self -> handleSelf
+        Literal v -> handleLiteral v
+        _ -> error "not implemented"
+
+handleSelf :: Interpreter ()
+handleSelf = push SelfV
+
+handleLiteral :: Value -> Interpreter ()
+handleLiteral v = push v
