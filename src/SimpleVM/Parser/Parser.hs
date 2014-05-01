467b00e0c7492d5da938c28fb0a4eb4774cebe50
Petesta <Petesta@live.com>
2014-04-30 17:56:34 -0700

    Rewriting some of the data types and their fields to accompany the send bytecode instruction. More fields may need to be added.

--- src/SimpleVM/Parser/Parser.hs
+++ src/SimpleVM/Parser/Parser.hs
@@ -3,11 +3,15 @@ module SimpleVM.Parser.Parser where
 import Control.Monad.State
 import Data.Maybe
 
---data InterpSTate = IS { stack :: [Value], pc :: Int }
+data Instr    = Self | Literal Value | Send Selector | SelfSend | SuperSend | Delegate | NonLocalReturn | IdxExtension
+data LitVal   = IntLit Int | StringLit String
+data Selector = Selector
+data Value    = SelfV | Method ObjData | Lit LitVal | Index Int
 
-data Instr = Self | Literal Value | Send | SelfSend | SuperSend | Delegate | NonLocalReturn | IdxExtension
-data Value = SelfV | Lit LitVal | Index Int
-data LitVal = IntLit Int | StringLit String
+data ObjData = ObjData {
+    mName   :: String,
+    numArgs :: Int
+}
 
 type InterpState = [Value]
 type Interpreter a = State InterpState a
@@ -36,14 +40,30 @@ handleInstruction instr =
     case instr of
         Self -> handleSelf
         Literal v -> handleLiteral v
+        Send s -> handleSend s
         _ -> error "not implemented"
 
+-- Push self onto the execution stack
 handleSelf :: Interpreter ()
 handleSelf = push SelfV
 
+-- Push literal value onto the execution stack
 handleLiteral :: Value -> Interpreter ()
 handleLiteral v = push v
 
--- Type signature subject to change
--- handleSend :: Interpreter ()
--- handleSend
+-- TODO: Determine what result to push and where it's located
+-- Find selector, top the stack and determine how many arguments
+-- need to be popped. Then push a result.
+handleSend :: Selector -> Interpreter ()
+handleSend s = do
+    -- selector <- selectorTable s
+    topElement <- top
+    case topElement of
+        Just value -> case value of
+            SelfV -> do
+                pop
+                push SelfV
+            Method (ObjData _ num) -> do
+                replicateM_ num pop
+                push SelfV
+        Nothing -> error "empty value from top"
