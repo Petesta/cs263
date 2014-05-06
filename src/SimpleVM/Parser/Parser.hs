ce40e8e2ead3cabe1f229bf5309710643bded546
Petesta <Petesta@live.com>
2014-05-05 18:32:43 -0700

    Changing up the send function and now push the result of the lookup onto the stack.

--- src/SimpleVM/Parser/Parser.hs
+++ src/SimpleVM/Parser/Parser.hs
@@ -3,18 +3,31 @@ module SimpleVM.Parser.Parser where
 import Control.Monad.State
 import Data.Maybe
 
-data Instr    = Self | Literal Value | Send Selector | SelfSend | SuperSend | Delegate | NonLocalReturn | IdxExtension
+import qualified Data.Map as M
+
+data Instr    = Self | Literal Value | Send Selector | SelfSend Selector | SuperSend | Delegate | NonLocalReturn | IdxExtension
 data LitVal   = IntLit Int | StringLit String
-data Selector = Selector
-data Value    = SelfV | Method ObjData | Lit LitVal | Index Int
+data Selector = Selector String
+data Value    = SelfV | Method ObjData | Lit LitVal | SendValue | Index Int
 
 data ObjData = ObjData {
     mName   :: String,
-    numArgs :: Int
+    numArgs :: Int,
+    vmt     :: VMT 
 }
 
-type InterpState = [Value]
+-- Virtual Method Table (Execution Stack)
+type VMT = M.Map Selector (Maybe Value)
+
+-- Operand Stack
+type InterpState   = [Value]
 type Interpreter a = State InterpState a
+
+instance Eq Selector where
+    (Selector s1) == (Selector s2) = s1 == s2
+
+instance Ord Selector where
+    compare (Selector s1) (Selector s2) = compare s1 s2
 
 top :: Interpreter (Maybe Value)
 top = do
@@ -35,13 +48,8 @@ push v = do
     stack <- get
     put (v:stack)
 
-handleInstruction :: Instr -> Interpreter ()
-handleInstruction instr =
-    case instr of
-        Self -> handleSelf
-        Literal v -> handleLiteral v
-        Send s -> handleSend s
-        _ -> error "not implemented"
+selectorTable :: M.Map Selector (Maybe Value) -> Selector -> Maybe Value
+selectorTable m s = fromMaybe (error "Method doesn\'t exist") $ M.lookup s m
 
 -- Push self onto the execution stack
 handleSelf :: Interpreter ()
@@ -51,19 +59,42 @@ handleSelf = push SelfV
 handleLiteral :: Value -> Interpreter ()
 handleLiteral v = push v
 
--- TODO: Determine what result to push and where it's located
--- Find selector, top the stack and determine how many arguments
--- need to be popped. Then push a result.
+-- TODO: send consumes the top of the stack and invokes a method (aka message) on it.
 handleSend :: Selector -> Interpreter ()
 handleSend s = do
-    -- selector <- selectorTable s
     topElement <- top
     case topElement of
         Just value -> case value of
             SelfV -> do
                 pop
                 push SelfV
-            Method (ObjData _ num) -> do
-                replicateM_ num pop
-                push SelfV
-        Nothing -> error "empty value from top"
+            Method (ObjData _ num vmt) -> do
+                pop
+                case selectorTable vmt s of
+                    Just method -> do
+                        replicateM_ num pop
+                        push method
+                    Nothing -> error "Method doesn\'t exist"
+        Nothing -> error "empty stack"
+
+-- TODO: Determine what result to push and where it's located
+-- Find selector and send selector to self on top of stack
+{-
+handleSelfSend :: Selector -> Interpreter ()
+hanldeSelfSend s = do
+    -- selector <- selectorTable s
+    topElement <- top
+    case topElement of
+        Just value -> do
+            pop
+            push SelfV -- TODO: not SelfV but needs to be result
+        Nothing -> error "empty stack"
+-}
+
+handleInstruction :: Instr -> Interpreter ()
+handleInstruction instr =
+    case instr of
+        Self -> handleSelf
+        Literal v -> handleLiteral v
+        Send s -> handleSend s
+        _ -> error "not implemented"
